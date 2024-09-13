51bf1988f63f8c4eefd6f9122a7cb4a30d1f36a1
kgcarroll <k_g_carroll@yahoo.com>
2024-09-13 14:00:48 -0400

    forgot a line of code that broke things, then ran a linter so this should work.

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,7 +10,6 @@ import {
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
 import { updateInvoice } from "@/app/lib/actions";
-import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
